5481adef346e6b541c45cbcab0b529d297889037
Rodrigo Quiñones Pichioli <rodrigo.q.p@hotmail.com>
2018-10-25 14:32:41 -0300

    Update common.js

--- client/src/utils/common.js
+++ client/src/utils/common.js
@@ -1,5 +1,5 @@
 /**
- * @description Used as callback the sort compare a with b based in a specified field, using bracket notation
+ * @description Used as callback, this function compares a with b and order them based in a specified field, using bracket notation
  * @param {object} a
  * @param {object} b
  * @param {string} field - The object property used to comparison
